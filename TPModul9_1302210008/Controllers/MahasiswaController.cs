60f75e89935252ee707294107c9a556ffc76ccce
adamrafiffaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-04-18 16:04:54 +0700

    add

---
+++ TPModul9_1302210008/Controllers/MahasiswaController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TPModul9_1302210008.Controllers
+{
+    [Route("api/[controller]")]
+    public class MahasiswaController : Controller
+    {
+        public static List<Mahasiswa> kelompok1 = new List<Mahasiswa>
+        {
+            new Mahasiswa("ADAM RAFIF FAQIH","1302210008"),
+            new Mahasiswa("MUHAMMAD ISA AL ANSHORI", "1302210014" ),
+            new Mahasiswa("HABLI ZULVANA ATH THAARIQ", "1302210024"),
+            new Mahasiswa("MAXIMUS BAYU PROUDIASTO", "1302210101"),
+            new Mahasiswa("MUHAMAD ILHAM NASRULLAH", "1302210041")
+        };
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return kelompok1;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return kelompok1[id];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Mahasiswa value)
+        {
+            kelompok1.Add(value);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            kelompok1.RemoveAt(id);
+        }
+    }
+}
+
---
+++ TPModul9_1302210008/Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TPModul9_1302210008
+{
+	public class Mahasiswa
+	{
+		public string nama { get; set; }
+		public string nim { get; set; }
+		public Mahasiswa() { }
+		public Mahasiswa(string nama, string nim)
+		{
+			this.nama = nama;
+			this.nim = nim;
+		}
+	}
+}
+
